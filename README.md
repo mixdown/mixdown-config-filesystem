73b9755522372f1b0de7cc8e774c444f227eeca4
Tommy Messbauer <tommy@vast.com>
2013-10-16 21:07:23 -0500

    added test for bad path.  added travis-ci

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ mixdown-config-filesystem
 =========================
 
 Mixdown configuration provider for local filesystem
+
+![travis-ci status](https://travis-ci.org/mixdown/mixdown-config-filesystem.png)
--- test/unit.js
+++ test/unit.js
@@ -21,30 +21,32 @@ suite('File System Config Plugin', function () {
       app.externalConfig.getServices(function(errServices, services) {
         
         assert.ifError(errServices);
-        console.error(services);
+        // console.error(services);
         done();
       });
     });
-
-
-    // var simpleApp = require('../../example/web/simple/mixdown.json');
-    // var mixdown = new Mixdown(simpleApp);
-
-    // assert.deepEqual(mixdown.config, simpleApp, 'Mixdown Instance should have its config set');
-
-    // mixdown.init(function(err) {
-
-    //   assert.ifError(err);
-    //   assert.ok(global.logger, 'Logger should be attached to global namespace');
-    //   assert.equal(mixdown.apps.length, 1, 'Mixdown should have generated 1 app');
-
-    //   var app = mixdown.apps[0];
-    //   assert.ok(app.plugins['hello-world'], 'Namespace should be initialized properly.');
-    //   assert.equal(app.plugins['hello-world'].hello, 'bonjour', 'An interface attr should be specified.');
-    //   assert.equal(app.plugins['hello-world'].sayHello(), 'bonjour', 'An interface attr should be specified.');
-
-    //   done();
-    // });
-
   });
+
+  test('Test with bad path', function (done) {
+
+    var app = new broadway.App();
+
+    app.use(new FilePlugin(), {
+      paths: ['/path/to/nowhere']
+    });
+
+    app.init(function(err) {
+
+      assert.ifError(err);
+      assert.ok( _.isFunction(app.externalConfig.getServices), "Get Services function should exist.");
+
+      app.externalConfig.getServices(function(errServices, services) {
+        
+        assert.ok(errServices);
+        // console.error(services);
+        done();
+      });
+    });
+  });
+
 });
